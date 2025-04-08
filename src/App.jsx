28004cc41a31b8f6091ca8aa41f7d41bb6a7619e
Anshul <anshuls26@gmail.com>
2025-04-08 14:05:21 +0530

    update with dynamic animations

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,43 @@
+import { useState } from "react";
 import "./App.css";
+import { useEffect } from "react";
 
 function App() {
-  const ProgressBar = ({progress})=>{
-    return <div className="outer">
-      <div className="inner" style={{width: `${progress}%`}}>{progress}%</div>
-    </div>
-  }
+  const bars = [1, 5, 10, 30, 40, 50, 70, 100];
+
+  const ProgressBar = ({ progress }) => {
+    const [animated, setAnimated] = useState(0);
+
+    useEffect(()=>{
+      setTimeout(()=> setAnimated(progress),100)
+    },[progress])
+    
+    return (
+      <div className="outer">
+        <div
+          className="inner"
+          style={{
+            // width: `${progress}%`,
+            transform: `translateX(${animated - 100}%)`,
+            backgroundColor: animated < 40 ? "black" : "green",
+          }}
+          role="progressbar"
+          aria-valuenow={progress}
+          aria-valuemax="100"
+          aria-valuemin="0"
+        >
+          {progress}%
+        </div>
+      </div>
+    );
+  };
 
   return (
     <>
       <h1>Progress Bar</h1>
-      <ProgressBar progress={70} />
+      {bars.map((value) => (
+        <ProgressBar key={value} progress={value} />
+      ))}
     </>
   );
 }
